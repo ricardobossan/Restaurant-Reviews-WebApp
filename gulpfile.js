5b7e0eeef5d12b646fb57722ea54b9a959d44603
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-10-25 05:50:41 -0300

    add browsersync package and configure it on the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,36 @@
+/**
+ * @file gulpefile.js. Task runner for JS.
+ * @desc A task runner for JS
+ */
+
+/**
+ * @desc Require dependencies.
+ * @requires
+ */
 const gulp = require('gulp');
 const jsdoc = require('gulp-jsdoc3');
+const bSync = require('browser-sync');
 
-// Generates documentation on the `doc` directory
-gulp.task('jsdoc', function (cb) {
+/**
+ * @desc Reloads the browser.
+ */
+gulp.task('default', () => {
+	gulp.watch("js/*.js").on('change', bSync.reload);
+	gulp.watch("css/*.css").on('change', bSync.reload);
+
+	bSync.init({
+		server: "./",
+		port: 8000,
+		index: "index.html",
+		ui: false
+	});
+});
+
+
+/*
+ * @desc Generates documentation on the `doc` directory.
+ */
+gulp.task('jsdoc', (cb) => {
 	gulp.src(['./README.md', './js/*.js'], {read: false})
 		.pipe(jsdoc(cb));
 });
