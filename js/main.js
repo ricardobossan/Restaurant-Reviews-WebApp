efdec96710a7f37489a314a7bd582dea1074f5b4
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-11-06 16:27:48 -0200

    add the 'alt' attribute to images

--- js/main.js
+++ js/main.js
@@ -183,6 +183,7 @@ createRestaurantHTML = (restaurant) => {
 	const image = document.createElement('img');
 	image.className = 'restaurant-img';
 	image.src = DBHelper.imageUrlForRestaurant(restaurant);
+	image.setAttribute("alt", "Restaurante image");
 	li.append(image);
 
 	const name = document.createElement('h1');
