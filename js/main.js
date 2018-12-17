a8a5749f866e3943ec639af5021addc125d15fe6
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-17 06:38:42 -0200

    update api keys to MapBox

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ initMap = () => {
 				scrollWheelZoom: false
 			});
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-		mapboxToken: 'pk.eyJ1Ijoic3RyaWRlcjg1IiwiYSI6ImNqbm5xeDNmdzI5YmszcW9qNWhnemF5NWQifQ.EALt7nbSvBWo-shZNNCtog',
+		mapboxToken: 'pk.eyJ1Ijoic3RyaWRlcjg1IiwiYSI6ImNqcHMxajBkajFnYzA0M3BndjBrOXN1aXkifQ.ahezmITWJqNHRNKhwJBzbw',
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
