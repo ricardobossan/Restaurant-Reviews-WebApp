bcb8748b745a256b07aedeec61bd026b8bce6e78
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-18 20:45:41 -0200

    DBHelper.DATABASE_URL's static method returns the location.origin + 'data/restaurant'

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const PORT = 5000
 express()
   .use(express.static(path.join(__dirname, '')))
   .get('/', (req, res) => res.render('index'))
+  .get('/data/restaurants', (req, res) => res.render('data/restaurants'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
